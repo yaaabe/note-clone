d1f49c231f5ae150bcbde5c53c83f9d4a2214978
yujiawoki <xyzxyzgod@gmail.com>
2025-09-30 13:52:11 +0900

    テスト修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
+テスト修正
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
